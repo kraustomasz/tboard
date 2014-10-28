4bb0770837a11c23352df29b57089e7fddd6446c
Tomasz Kraus <tomasz.kraus@brainly.com>
2014-10-28 12:28:49 +0000

    twocolumn view

--- app/assets/javascripts/user_places.js
+++ app/assets/javascripts/user_places.js
@@ -1,7 +1,7 @@
 var cur_row = 0;
 var timeout = 10000;
 var scroll_by = 15;
-var scroll_time = 1000;
+var scroll_time = 3000;
 var timeout_handler;
 function next_scroll() {
 	if (cur_row < max_rows - scroll_by) {
@@ -15,11 +15,27 @@ function next_scroll() {
 	timeout_handler = setTimeout('next_scroll();', timeout)
 }
 
+function adjust_uservoice_height() {
+	var window_height = window.innerHeight;
+	if (window.innerWidth < 1000) {
+		$('#uservoice').css('height', '');
+		return;
+	}
+	var header_height = $('#uservoice-wrapper .all-header').height();
+	$('#uservoice').height(window_height - header_height);	
+}
+
 $(function() { 
 	$('body').bind('click', function() {
 		clearTimeout(timeout_handler);
 	});
-	timeout_handler = setTimeout('next_scroll();', timeout)
+	if (is_tv) {
+		timeout_handler = setTimeout('next_scroll();', timeout)
+	}
+	
+	adjust_uservoice_height();
 
-
+	$(window).on('resize', function(){
+		adjust_uservoice_height();
+	});
 });
